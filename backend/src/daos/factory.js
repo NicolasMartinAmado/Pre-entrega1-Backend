9702cb932bb398a898d247d65b17b86df8b602e6
Nicolas Martin Amado <nikiamado123@gmail.com>
2024-05-13 14:54:02 -0300

    render web

--- backend/src/daos/factory.js
+++ backend/src/daos/factory.js
@@ -1,4 +1,4 @@
-const { configObject } = require("../config/config")
+const { configObject } = require("../config/config.js")
 
 let UserDao
 let ProductDao
@@ -9,24 +9,24 @@ let CartFile
 
 switch (configObject.persistence) {
     case 'MONGO':
-        const UserDaoMongo = require('./mongo/userDaoMongo')
+        const UserDaoMongo = require('./mongo/userDaoMongo.js')
         UserDao = UserDaoMongo
 
-        const ProductDaoMongo = require('./mongo/productDaoMongo')
+        const ProductDaoMongo = require('./mongo/productDaoMongo.js')
         ProductDao = ProductDaoMongo
 
-        const CartDaoMongo = require('./mongo/cartDaoMongo')
+        const CartDaoMongo = require('./mongo/cartdaomongo.js')
         CartDao = CartDaoMongo
 
-        const MessageDaoMongo = require('./mongo/messageDaoMongo')
+        const MessageDaoMongo = require('./mongo/messageDaomongo.js')
         MessageDao = MessageDaoMongo
         break;
 
     case 'FILE':
-        const ProductFileManager = require('./file/productManagerfile')
+        const ProductFileManager = require('./file/productManagerFile.js')
         ProductFile = ProductFileManager
 
-        const CartFileManager = require('./file/cartManagerfile')
+        const CartFileManager = require('./file/cartManagerFile.js')
         CartFile = CartFileManager
         break;
 
