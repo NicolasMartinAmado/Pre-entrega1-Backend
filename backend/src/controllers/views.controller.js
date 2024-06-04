67c5ee80f0537a4403b007662309fe9f02fd9255
Nicolas Martin Amado <nikiamado123@gmail.com>
2024-06-04 17:25:15 -0300

    res

--- backend/src/controllers/views.controller.js
+++ backend/src/controllers/views.controller.js
@@ -258,7 +258,7 @@ console.log(docs)
                 return res.status(400).json({ error: 'Token is no valid or expired' })
             }
     
-            const user = await this.userViewService.getUserBy(decodedToken.userId)
+            const user = req.session.user;
             if (!user) {
                 return res.status(400).json({ error: 'User not found' })
             }
